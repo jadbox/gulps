bf82f95ca7a60955844af93f540b2c6bebf846e3
KFlash <kflash123@gmail.com>
2015-12-04 15:31:41 +0800

    package.json fixes

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -1,6 +1,5 @@
 import gulp from 'gulp';
 import _mocha from '../util/_mocha'; // shared between coverage and this file
-import runSequence from 'run-sequence';
 
 gulp.task('test', ['lint:test'], function() {
     return _mocha();
